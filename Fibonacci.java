011bfb899613d199ba40d67a6afed4249a368850
Rohit Narayan <rohit.narayan@flipkart.com>
2018-10-02 15:29:39 +0530

    fibonacci modified

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,26 +1,19 @@
-//Java program for Fibonacci Series using Space 
-class fibonacci 
-{ 
-    static int fib(int n) 
-    { 
-        /* Declare an array to store Fibonacci numbers. */
-    int f[] = new int[n+2]; // 1 extra to handle case, n = 0 
-    int i; 
-       
-    /* 0th and 1st number of the series are 0 and 1*/
-    f[0] = 0; 
-    f[1] = 1; 
-      
-    for (i = 2; i <= n; i++) 
-    { 
-       /* Add the previous 2 numbers in the series 
-         and store it */
-        f[i] = f[i-1] + f[i-2]; 
-    } 
-       
-    return f[n]; 
-    
-    } 
+import java.util.Scanner;
+
+//Java program for Fibonacci Series using loop in time complexity O(n) and space complexity O(1)
+class fibonacci
+{
+    static int fib(int n) {
+        int fibNum2 = 0;
+        int fibNum1 = 1;
+        int fibSum = 0;
+        for(int i = 2; i <= n; i++) {
+            fibSum = fibNum1 + fibNum2;
+            fibNum2 = fibNum1;
+            fibNum1 = fibSum;
+        }
+        return fibSum;
+    }
   
     public static void main (String args[]) 
     { 
